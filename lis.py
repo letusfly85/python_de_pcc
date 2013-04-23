8567139bb9077397f6f9808c72fe761e68ef53f7
shunsuke wada <letusfly95@gmail.com>
2013-04-24 00:20:42 +0900

    #python 最長増加部分列問題

---
+++ lis.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+Num = 5
+A = [4, 2, 3, 1, 5]
+
+memsets = {}
+
+def initialize_memsets():
+    memsets[0] = 0
+
+def solve():
+    for i in range(0, Num):
+        memsets[i+1] = 1
+        
+        for j in range(0, i):
+            if (A[j] < A[i]):
+                memsets[i+1] = max(memsets[i+1], memsets[j]+1)
+
+    print memsets[Num]
+
+def __main__():
+    initialize_memsets()
+    solve()
+
+__main__()
