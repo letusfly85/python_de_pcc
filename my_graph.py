774e2d1ccc530dc9611b87e86904269c5b48704c
shunsuke wada <letusfly95@gmail.com>
2013-05-09 00:43:35 +0900

    #python クラス追加

--- my_graph.py
+++ my_graph.py
@@ -8,3 +8,24 @@ class MyGraph:
 
     def set_neighbor(self, n):
         self.neighbors.append(n)
+
+    def set_neighbors(self, ns):
+        for i in range(0, len(ns)):
+            self.set_neighbor(ns[i])
+
+    def who_am_i(self, n, ns):
+        for i in range(0, len(ns)):
+            if (n == ns[i].node):
+                return ns[i]
+
+class MyEdge:
+    def __init__(self, nf, nt, c):
+        self.node_from = nf 
+        self.node_to   = nt
+        self.cost      = c
+
+
+class MyPair:
+    def __init__(self, sd, n):
+        self.shortest_distance = sd
+        self.node              = n
